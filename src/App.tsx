cb9b41f4d2753fe23202a7cf6480df87b698cc7e
Christian Sandoval <christiansandoval1018@outlook.com>
2023-05-07 21:59:40 -0500

    started styling cards

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,21 @@
-import './App.css';
+import './App.scss';
 import { cardsData } from './cardsData';
 import Card from './components/Card';
 
 function App() {
 	return (
 		<div className='App'>
-			{cardsData.map((card) => (
-				<Card
-					type={card.type}
-					date={card.date}
-					title={card.title}
-					tags={card.tags}
-					author={card.author}
-				></Card>
-			))}
+			<div className='card-layout'>
+				{cardsData.map((card) => (
+					<Card
+						type={card.type}
+						date={card.date}
+						title={card.title}
+						tags={card.tags}
+						author={card.author}
+					></Card>
+				))}
+			</div>
 		</div>
 	);
 }
